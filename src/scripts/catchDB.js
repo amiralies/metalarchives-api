4a20bdf9ebe18fe16b3844285105da90b85f2c51
Amirali Esmaeili <esmailiamirali@gmail.com>
2017-12-26 00:58:49 +0330

    Improve db catch script performance

--- src/scripts/catchDB.js
+++ src/scripts/catchDB.js
@@ -5,25 +5,14 @@ const cheerio = require('cheerio');
 const config = require('../helpers/config');
 require('../models/band');
 
+const Band = mongoose.model('Band');
 const MA_URL =
   'https://www.metal-archives.com/search/ajax-advanced/searching/bands/?iDisplayStart=';
-
 const startTime = Date.now();
 
 console.log('Catching...');
 
-mongoose.Promise = Promise;
-mongoose.connect(config.DB_CONNECTION_STRING, {
-  useMongoClient: true,
-});
-const Band = mongoose.model('Band');
-
-const requestBands = index => axios.get(MA_URL + (index * 200).toString());
-
-const getBands = requestArr =>
-  Promise.map(requestArr, item => requestBands(item));
-
-const saveBands = (bands) => {
+const saveBands = bands => new Promise((resolve, reject) => {
   const bandsToSave = [];
   bands.forEach((band) => {
     const $ = cheerio.load(band[0]);
@@ -36,9 +25,29 @@ const saveBands = (bands) => {
     };
     bandsToSave.push(bandObj);
   });
-  return Band.insertMany(bandsToSave, { ordered: false });
-};
+  Band.insertMany(bandsToSave, { ordered: false })
+    .then((res) => { resolve(res.length); })
+    .catch((err) => {
+      if (err.code === 11000) {
+        resolve(0);
+      } else reject(err);
+    });
+});
 
+const requestBands = index => new Promise((resolve, reject) => {
+  axios.get(MA_URL + (index * 200).toString())
+    .then(({ data }) => {
+      const bands = data.aaData;
+      return saveBands(bands);
+    })
+    .then((res) => {
+      resolve(res);
+    })
+    .catch(err => reject(err));
+});
+
+const getBands = requestArr =>
+  Promise.map(requestArr, item => requestBands(item), { concurrency: 3 });
 
 const main = () => {
   axios
@@ -53,12 +62,8 @@ const main = () => {
       return getBands(requestArr);
     })
     .then((res) => {
-      const data = res.map(item => item.data.aaData);
-      const bands = [];
-      data.forEach((item) => {
-        bands.push(...item);
-      });
-      return saveBands(bands);
+      const newBandsCount = res.reduce((sum, item) => sum + item);
+      console.log(`Added ${newBandsCount} new bands.`);
     })
     .catch((err) => {
       if (err.code !== 11000) {
@@ -66,10 +71,17 @@ const main = () => {
       }
     })
     .then(() => {
-      console.log(`Done.\nTotal Time spent : 
+      console.log(`Total Time spent : 
       ${(Date.now() - startTime).toString()}ms`);
       mongoose.connection.close();
     });
 };
 
-main.call();
+mongoose.Promise = Promise;
+mongoose.connect(config.DB_CONNECTION_STRING, {
+  useMongoClient: true,
+}, (err) => {
+  if (err) {
+    console.log(`MongoDB Connection Error: ${err}`);
+  } else main.call();
+});
